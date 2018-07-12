4937e8f9d191c90a2b80a9814a4b498c86aee3b3
lucassantosf <lucas46em@gmail.com>
2018-07-12 20:48:39 -0300

    Estrutura condicional e laços de repetições

---
+++ condicionais-lacosRepeticoes/foreach.php
@@ -0,0 +1,8 @@
+<?php
+	$meses = array("Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Setembro","Outubro","Novembro","Dezembro");
+
+	foreach($meses as $index=>$mes){
+		echo "Indice".$index."<br>";
+		echo "O mes é : ".$mes."<br>"; 
+	}
+?>
