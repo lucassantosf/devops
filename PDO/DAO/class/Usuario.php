ae64d7a2f14691abe8b0cfef0daeb102682102ad
lucassantosf <lucas46em@gmail.com>
2018-09-08 16:04:52 -0300

    Estudo de PDO com PHP

--- PDO/DAO/class/Usuario.php
+++ PDO/DAO/class/Usuario.php
@@ -27,7 +27,7 @@ class Usuario {
 		return $this->dessenha;
 	}
 
-	public function setDesenha($value){
+	public function setDessenha($value){
 		$this->dessenha = $value;
 	}
 
@@ -43,19 +43,113 @@ class Usuario {
  
 		$sql = new Sql();
 	 
-		$result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+		$result = $sql->select("SELECT * FROM tb_usuarios WHERE id_usuario = :ID", array(
 			":ID" => $id
 		));
 	 
 		if(count($result) > 0) {
-			$row = $result[0];
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			
+			$this->setData($result[0]);
 	 
 		}
  
+	}
+
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * from tb_usuarios ORDER BY deslogin;");
+
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+		return $sql->select("SELECT * from tb_usuarios where deslogin like :SEARCH order by deslogin ", array(
+                ':SEARCH'=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+	 
+		$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN" => $login,
+			":PASSWORD" => $password
+		));
+	 
+		if(count($result) > 0) {
+			
+			$this->setData($results[0]);
+
+		}else{
+
+			throw new Exception("Login e/ou senha inválidos");
+			
+		}
+
+	}
+
+	public function setData($data){
+
+		$this->setIdusuario($data['id_usuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+
+	}
+
+	public function insert(){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+				':LOGIN'=>$this->getDeslogin(),
+				':PASSWORD'=>$this->getDessenha()
+		));
+
+		if(count($results)>0){
+			$this->setData($results[0]);
+		}
+
+	}
+
+	public function __construct($login = "",$password = ""){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+	}
+
+	public function update($login, $password){
+
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_usuarios set deslogin = :LOGIN, dessenha = :PASSWORD where id_usuario = :ID", arraY(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha(),
+			':ID'=>$this->getIdusuario()
+		));
+
+	}
+
+	public function delete(){
+
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_usuarios where id_usuario = :ID", array(
+			':ID'=>$this->getIdusuario()
+		));
+
+		$this->setIdusuario(0);
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtcadastro(new DateTime());
+
 	}
 
 	public function __toString() {
@@ -67,11 +161,11 @@ class Usuario {
 	    }
 	 
 	    return json_encode(array(
-	            "idusuario"=>$this->getIdusuario(),
+	            "id_usuario"=>$this->getIdusuario(),
 	            "deslogin"=>$this->getDeslogin(),
 	            "dessenha"=>$this->getDessenha(),
 	            "dtcadastro"=> $data
-    	));
+	    ));
 	} 
 }
 
--- PDO/DAO/index.php
+++ PDO/DAO/index.php
@@ -2,10 +2,41 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega apenas um usuário
+//$root = new Usuario();
+//$root->loadById(21);
+//echo $root;
 
-$root->loadById(4);
+//Carregar uma lista de usuários
+//lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
 
+//Carregar uma lista de usuários buscando pelo login
+//$search = Usuario::search("o");
+//echo json_encode($search);
+
+//Fazer login carregando apenas um usuário
+//$usuario = new Usuario();
+//$usuario->login("llla","newSenhaalterada");
+//echo $usuario;
+
+//Insert de novo usuário
+//$aluno = new Usuario("aluno","45");
+//$aluno->insert();
+//echo $aluno;
+
+/*Editar um usuário existente
+$usuario = new Usuario();
+$usuario->loadById(20);
+$usuario->update("TEACHER","111233334");
+echo $usuario;*/
+
+//Deletar um usuário
+$usuario = new Usuario();
+$usuario->loadById(20);
+
+$usuario->delete();
+
+echo $usuario;
 ?>
