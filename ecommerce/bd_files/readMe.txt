a6dc61ad91d2784705696e9b8af8bfb95955d3b0
lucassantosf <lucas46em@gmail.com>
2018-11-06 22:04:15 -0200

    Configurações iniciais template Admin e Site

---
+++ ecommerce/bd_files/readMe.txt
@@ -0,0 +1,14 @@
+Arquivos do Banco de Dados deste projeto
+
+Segue em ordem de execução e alterações realizadas para o desenvolvimento do Ecommerce.
+
+1  - Modelagem inicial do projeto
+2  -  
+3  - 
+4  -
+5  -
+6  -
+7  - 
+8  -
+9  -
+10 -
--- ecommerce/index.php
+++ ecommerce/index.php
@@ -2,14 +2,26 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\DB\Sql;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
 
+$app = new Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	$page = new Page();
 
+	$page->setTpl("index");
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
 });
 
 $app->run();
