60de4f1800f9d5b9a50edef051e79291b19f6b54
Lucas <lucas46em@gmail.com>
2022-10-19 19:08:08 -0300

    init new flow register

--- k6/register.js
+++ k6/register.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const base_url = 'https://domain.com.br/api/'
+const base_url = 'https://sandbox.domain.com.br/api/'
 var params = { headers: { 'Content-Type': 'application/json'}}
 
 var campanha_id;
@@ -23,6 +23,10 @@ const GET = (route) => {
 export default function () {
   check_campanha()
   check_document()
+  // check_account() **todo
+  // validate_cpf()  **todo
+  // consult_cnpj()  **todo
+  // register()      **todo
   login()
   me()
   cartoes()
@@ -40,6 +44,61 @@ function check_document(){
   const payload = JSON.stringify({document: '',campanha: 'DEVS'});
   const res = POST('check-document', payload);
   check(res, { 'document': (r) => r.status == 200 });
+  sleep(random())
+} 
+
+function check_account(){
+  const payload = JSON.stringify({document: '',campanha: 'DEVS', account: ''});
+  const res = POST('account-campanha', payload);
+  check(res, { 'document': (r) => r.status == 200 });
+  sleep(random())
+}
+
+function validate_cpf(){
+  const payload = JSON.stringify({document: '',campanha: 'DEVS', account: ''});
+  const res = POST('account-campanha', payload);
+  check(res, { 'document': (r) => r.status == 200 });
+  sleep(random())
+}
+
+function consult_cnpj(){
+  const payload = JSON.stringify({document: '',campanha: 'DEVS', account: ''});
+  const res = POST('account-campanha', payload);
+  check(res, { 'document': (r) => r.status == 200 });
+  sleep(random())
+}
+
+function register(){
+  // "tipo":"fisica",
+	// "document":"",
+	// "campanha":"",
+	// "name":"Dede", 
+	// "nome_cpf":"Fred",
+	// "nome_da_mae":"Maria",
+	// "apelido":"Neto",
+	// "genero":"MASCULINO",
+	// "data_nascimento":"1990-01-31",
+	// "phone_number":"5511981...", /** unique, gerar function p/ gerar unique */
+	// "email":"@", /** unique, gerar function p/ gerar unique */
+	// "password":"12345678",
+	// "password_confirmation":"12345678",
+	// "cep":"18078600",
+	// "nome":"Rua Juvencio das Flores",
+	// "numero":"50",
+	// "complemento":"",
+	// "bairro":"Jardim das Flores",
+	// "cidade":"Sorocaba",
+	// "estado":"SP",
+	// "sms_service":1,
+	// "aceita_sms":1,
+	// "aceita_whats":1,
+	// "aceita_email":1,
+	// "aceita_termos":1,
+	// "aceita_politicamente_exposta":1
+
+  const payload = JSON.stringify({document: '', password: ''});
+  const res = POST('register', payload);
+  check(res, { 'login': (r) => r.status == 200 });
   sleep(random())
 } 
 
