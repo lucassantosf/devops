f2e5bd991a11a7f680ba34f6b749ed24184d7b57
Lucas <lucas46em@gmail.com>
2023-12-01 12:44:06 -0300

    Study python language

---
+++ python/study/if.py
@@ -0,0 +1,9 @@
+nota = input('Informe sua nota')
+nota = int(nota)
+
+if (nota >= 7):
+    print('Aprovado')
+elif nota == 6 :
+    print('Recuperacao')
+else:
+    print('Reprovado')
---
+++ python/study/vars.py
@@ -0,0 +1,13 @@
+x = 1
+y = 1.3
+z = x+y
+n = 'name'
+print(z)
+print(type(x),type(y),type(z),type(n))
+
+i = input('Informe valor:') # input can be empty
+print(float(i)+z)
+
+'''
+coments on many line
+'''
